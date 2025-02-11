7bf0770c72edb9b3cacb7036778313e4adc4f330
suonabera <71337148+suonabera@users.noreply.github.com>
2025-02-11 15:07:36 +0100

    Add MSVC 9 support

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include "SDL.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#define SDL_main main
 
-int main() {
+int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	printf("SDL Video Initialized\n");
 	const SDL_VideoInfo *vidinfo = SDL_GetVideoInfo();
 	printf("SDL_GetVideoInfo success\n");
-	printf("current_w: %i\n", vidinfo->current_w);
-	printf("current_h: %i\n", vidinfo->current_h);
+	char output[128];
+	sprintf(output, "current_w: %i, current_h: %i", vidinfo->current_w, vidinfo->current_h);
+	printf(output);
+#ifdef _WIN32
+	MessageBoxA(0, output, "sdlresinfo", 0x0000040L);
+#endif
 }
